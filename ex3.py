391ccf75eb3bd999cfefbddff8549676cef6fc05
rafaelyamaguchicavalcante <ycavalcanterafael@gmail.com>
2025-03-08 19:51:16 -0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,7 +0,0 @@
-#3) Elaborar um algoritmo que solicite os dados de estatura (em metros) e peso (em Kg) de
-#uma pessoa e calcule/visualize seu IMC (Índice de Massa Corporal):
-estatura = float(input("Digite sua estatura:"))
-peso = float(input("Digite seu peso:"))
-
-resultado = peso / estatura ** 2
-print(resultado)
