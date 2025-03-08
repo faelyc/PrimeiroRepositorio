319caaef3623359b95a8b962cbfbd748b521f31c
rafaelyamaguchicavalcante <ycavalcanterafael@gmail.com>
2025-03-08 19:50:46 -0300

    Delete ex16.py

--- ex16.py
+++
@@ -1,8 +0,0 @@
-# 16 Entre com dois números e exiba as comparações:
-
-num1 = 2
-num2 = 3
-
-print("Primeiro número é maior que o segundo?", num1 > num2)
-print("Os dois números são iguais?", num1 == num2)
-print("Primeiro número é menor ou igual ao segundo?", num1 <= num2)
